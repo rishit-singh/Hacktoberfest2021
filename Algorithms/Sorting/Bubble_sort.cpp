3174c3335baa2d994980d4352ffb0644c58ad0f1
Kartikay123 <78485196+Kartikay123@users.noreply.github.com>
2021-10-17 18:18:46 +0530

    Create Bubble_sort.cpp

---
+++ Algorithms/Sorting/Bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+// In the worst Case time complexity is O(n^2) and in best case it is O(n);
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
