12fd2c367ca6f490ec4a7fb60d96addb1f57d433
rishit-singh <rsrishitsingh@gmail.com>
2021-10-18 18:28:51 +0530

    Added a Cos function.

---
+++ Math/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "trigfuncs.h"
+
+int main(int argc, char** argv)
+{
+    printf("%f\n", Cos(atof(argv[1])));
+
+    return 0;
+}
--- Math/trigfuncs.h
+++ Math/trigfuncs.h
@@ -8,19 +8,28 @@
 #include "misc.h"
 
 double CalculateSin(double, const int); // Taylor series approximation based sine function that returns the sine value of the provided angle in radians.
-double Sin(double); // Wrapper function for CalculateSin(double, const int);
+double Sin(double); // Wrapper function for CalculateSin(double, const int), takes angle in degrees as input.
+
+double CalculateCos(); // Taylor series approximation based cosine function that returns the cosine value of the provided angle in degrees.
+double Cos(double); // Wrapper function for CalculateCos(double, const int), takes angle in degrees as input.
 
 double Cosec(double);
-double Cos(double); // Taylor series approximation based cosine function that returns the cosine value of the provided angle in degrees.
 double Sec(double);
 double Tan(double);
 double Cot(double);
 
 int GetRecurranceCount(double); // Finds the cyclic function recurrance count for angles in radians.
 
+int GetSign(const int);  // Returns a signed or unsigned value for the taylor series depending upon the provided value.
+
 int GetRecurranceCount(double angle)
 {
     return (angle / (PI));
+}
+
+int GetSign(const int n)
+{
+    return (n % 2 == 1) * -1 | (!(n % 2));
 }
 
 double CalculateSin(double angle, const int maxIterations)
@@ -30,7 +39,7 @@ double CalculateSin(double angle, const int maxIterations)
         factorial = 0;
 
     const int n = GetRecurranceCount(angle), // Gets the repeatation of PI
-        sign = (n % 2 == 1) * -1 || (!(n % 2)); // Quadrant sign
+        sign = GetSign(n); // Quadrant sign
 
     angle -= n * PI;
 
@@ -50,5 +59,31 @@ double Sin(double angle)
     return CalculateSin(RADIANS(angle), 10);
 }
 
+double CalculateCos(double angle, int maxIterations)
+{
+    double cosval = 0,
+        numerator = 0,
+        factorial = 0;
+
+    const int n = GetRecurranceCount(angle), // Gets the repeatation of PI
+        sign = GetSign(n); // Quadrant sign
+
+    angle -= n * PI;
+
+    for (int x = 0; x < maxIterations; x++)
+    {
+        numerator = pow(angle, 2 * x);
+        factorial = Factorial(2 * x);
+
+        cosval += pow(-1, x) * (numerator / factorial);
+    }
+
+    return (sign * cosval);
+}
+
+double Cos(double angle)
+{
+    return CalculateCos(RADIANS(angle), 10);
+}
 
 #endif
